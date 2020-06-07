9b530714405b627d81cfd009a8e6d1e69cd455e6
Kirill Starostin <kirill.starostin@gost-group.com>
2020-06-07 14:53:57 +0300

    echo add

--- diskspace/diskspace.php
+++ diskspace/diskspace.php
@@ -6,6 +6,8 @@ while(TRUE) {
  $disk_free = disk_free_space("E:");
  $disk_all = disk_total_space("E:");
  $percent = round(($disk_free / $disk_all) * 100);
+ sleep(1);
+ echo "On disk E:\ free $percent%\n";
  if ($percent < 10) {
   $WshShell->Popup("А место-то на диске... тютю... ( меньше $percent%", 0, "АХТУНГ!", 4096+64);
  }
