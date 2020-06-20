4c1023e6be7ecdde9b4d6e18af99b4f670309748
superlab <superlab@anabot.ru>
2020-06-20 16:12:03 +0300

    scale

---
+++ scale/scale.bat
@@ -0,0 +1,2 @@
+chcp 1251
+start "Ã¿——¿" "cmd /c .\php\php.exe scale.php"
---
+++ scale/scale.php
@@ -0,0 +1,42 @@
+<?php
+include "includes/phpSerial.php";
+use phpSerial\phpSerial;
+
+// Let's start the class
+$serial = new phpSerial;
+
+
+$serial->_os = "windows";
+
+
+// First we must specify the device. This works on both linux and windows (if
+// your linux serial device is /dev/ttyS0 for COM1, etc)
+$serial->deviceSet("COM1");
+
+// We can change the baud rate, parity, length, stop bits, flow control
+$serial->confBaudRate(2400);
+$serial->confParity("even");
+$serial->confCharacterLength(8);
+$serial->confStopBits(1);
+$serial->confFlowControl("none");
+
+// Then we need to open it
+$serial->deviceOpen();
+
+// To write into
+$serial->sendMessage("Hello !");
+
+// Or to read from
+$read = "";
+while(true) {
+  usleep(1);
+  $read = $serial->readPort();
+  if(preg_match("#.*g.*#isU",$read)) {
+  $res = (float)$read;
+  if ($res <= 201) $res = number_format($res,3);
+  else $res = number_format($res,2);
+  if ($res > 0.005 && $res_tmp != $res) echo $res . "\n";
+  $res_tmp = $res;
+  }
+}
+?>
